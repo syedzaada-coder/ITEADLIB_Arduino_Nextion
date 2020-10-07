df384d39687c1371db085046d1f3b0375dc947e6
syedzaada-coder <72509760+syedzaada-coder@users.noreply.github.com>
2020-10-07 19:28:21 +0500

    Update NexConfig.h
    i need it for mega 2560 with nextion comunication

--- NexConfig.h
+++ NexConfig.h
@@ -24,7 +24,7 @@
  * Define DEBUG_SERIAL_ENABLE to enable debug serial. 
  * Comment it to disable debug serial. 
  */
-#define DEBUG_SERIAL_ENABLE
+//#define DEBUG_SERIAL_ENABLE
 
 /**
  * Define dbSerial for the output of debug messages. 
@@ -34,7 +34,7 @@
 /**
  * Define nexSerial for communicate with Nextion touch panel. 
  */
-#define nexSerial Serial2
+#define nexSerial Serial1
 
 
 #ifdef DEBUG_SERIAL_ENABLE
